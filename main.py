509095e57b7c66cea59570057eb62269b0006a00
yblockchain_ <144770048+yblockcha1n@users.noreply.github.com>
2023-09-15 21:02:53 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,71 +2,58 @@ import discord
 from discord.ext import commands
 import os
 import openai
-import asyncio
-
-token = "YOUR_BOT_TOKEN"
-os.environ['TOKEN'] = token
-
-openai_apikey = "OPENAI_API_KEY"
-os.environ['OPENAI_API_KEY'] = openai_apikey
-
 from langchain.prompts import (
-  ChatPromptTemplate, 
-  MessagesPlaceholder, 
-  SystemMessagePromptTemplate, 
-  HumanMessagePromptTemplate
+    ChatPromptTemplate, 
+    MessagesPlaceholder, 
+    SystemMessagePromptTemplate, 
+    HumanMessagePromptTemplate
 )
 from langchain.chains import ConversationChain
 from langchain.chat_models import ChatOpenAI
 from langchain.chains.conversation.memory import ConversationSummaryBufferMemory
 
+# Enter discord bot token and OpenAI KEY
+os.environ['TOKEN'] = "YOUR_BOT_TOKEN"
+os.environ['OPENAI_API_KEY'] = "OPENAI_API_KEY"
+
+# Discord Bot Setting
 intents = discord.Intents.default()
 intents.message_content = True
-client = discord.Client(intents=intents)
+bot = commands.Bot(command_prefix="/", intents=intents)
 
-token = os.environ['TOKEN']
 openai.api_key = os.environ['OPENAI_API_KEY']
-
-system_settings = """あなたはWeb3やIT技術に詳しいアシスタント型AIです。""" #AI characteristics can be changed arbitrarily
+system_settings = """あなたはWeb3やIT技術に詳しいアシスタント型AIです。""" # Bot characteristics can be changed at will :)
 prompt = ChatPromptTemplate.from_messages([
-  SystemMessagePromptTemplate.from_template(system_settings),
-  MessagesPlaceholder(variable_name="history"),
-  HumanMessagePromptTemplate.from_template("{input}")
+    SystemMessagePromptTemplate.from_template(system_settings),
+    MessagesPlaceholder(variable_name="history"),
+    HumanMessagePromptTemplate.from_template("{input}")
 ])
-
-use_model = "gpt-3.5-turbo" #GPT version can also be changed arbitrarily
+use_model = "gpt-3.5-turbo"
 S_conversation = ConversationChain(
-  memory=ConversationSummaryBufferMemory(
-    return_messages=True,
+    memory=ConversationSummaryBufferMemory(
+        return_messages=True,
+        llm=ChatOpenAI(model_name=use_model),
+        max_token_limit=1000
+    ),
+    prompt=prompt,
     llm=ChatOpenAI(model_name=use_model),
-    max_token_limit=1000 #Number of characters a bot can send
-  ),
-  prompt=prompt,
-  llm=ChatOpenAI(model_name=use_model),
-  verbose=True
+    verbose=True
 )
 
-intents = discord.Intents.default()
-intents.message_content = True
-
-bot = commands.Bot(
-    command_prefix="/",
-    intents=intents
-)  
-
+# Signal when the bot is activated
 @bot.event
 async def on_ready():
-    print(f'{bot.user}が正常に実行されました') #Bot startup log (terminal)
+    print(f'{bot.user}が正常に実行されました')
 
+# /ask command processing
 @bot.command()
 async def ask(ctx, *, question):
-    async with ctx.typing(): #Typing animation
+    async with ctx.typing():
         S_text = S_conversation.predict(input=question)
         await ctx.send(S_text)
+       
+      # Save memory log to file (If it does not exist, it will be created automatically)
+        with open('memory.txt', 'w') as Sf:
+            Sf.write(str(S_conversation.memory.load_memory_variables({})))
 
-        S_memory_text = S_conversation.memory.load_memory_variables({})
-        Sf = open('memory.txt', 'w') #A log of the conversation is recorded in memory.txt (*If not created, it will be automatically added to the file)
-        Sf.write(str(S_memory_text))
-        Sf.close()
-
-bot.run(token)
+bot.run(os.environ['TOKEN'])
